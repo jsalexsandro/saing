b409058f65af0fcf57eadea037b5a717847ac7a0
jsalexsandro <eujosealexsandro@gmail.com>
2022-11-27 14:25:31 -0300

    fix:Added Script Ads Url

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document(){
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
         <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet"/>
         <meta name="google-site-verification" content="XNgvOQq81TS-u9Dc1RpLttMMU0lG_vlRhH16PyZNCOo" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5229654791822120" crossOrigin="anonymous"></script>
         <title>
           SAING.NET
         </title>
